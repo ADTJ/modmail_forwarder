fa7d1835d64ef068799871dbaec4ace7d000a9da
Aidy <microsoft@aidanjenkins.co.uk>
2020-01-24 23:42:42 +0000

    Deleted output js and map files

--- Modmail_Forwarder/forwarder.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Forwarder {
-    constructor(config) {
-    }
-}
-exports.Forwarder = Forwarder;
-//# sourceMappingURL=forwarder.js.map
